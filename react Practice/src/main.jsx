3fcaffc09de439d597a0942b27403a024a996fdc
SivaPrasad Reddy Devloper <evurisivaprasadreddy3@gmail.com>
2025-11-04 13:52:17 +0530

    changes

--- react Practice/src/main.jsx
+++ react Practice/src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import "./App.css"
+
+import Header from "./landing pages/header.jsx"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
+import Banner from './landing pages/banner.jsx'
+import Cards from './landing pages/main.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Header />
+    <Banner/>
+    <Cards/>
   </StrictMode>,
 )
